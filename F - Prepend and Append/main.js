3e4cf852fdb8015b00d26b8e9c66f847fc1b4612
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-02-13 19:00:53 +0200

    add files

---
+++ F - Prepend and Append/main.js
@@ -0,0 +1,35 @@
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+function print(x) {
+  console.log(x);
+}
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+process.stdin.on("end", () => {
+  inputString = inputString.split("\n");
+  main();
+});
+function readline() {
+  return inputString[currentLine++];
+}
+
+// ********** Code Start **********
+
+function main() {
+  let t = parseInt(readline());
+  for (let i = 0; i < t; i++) {
+    let n = parseInt(readline());
+    let s = readline().trim().split("");
+    while (s.length>1 && s[0] != s[s.length -1]) {
+      s.pop();
+      s.shift();
+    }
+    print(s.length);
+  }
+}
