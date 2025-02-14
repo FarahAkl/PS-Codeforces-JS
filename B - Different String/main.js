56dbaa53fb9e5071ec1f256226176f32b0b8f539
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-02-14 13:35:05 +0200

    add files

---
+++ B - Different String/main.js
@@ -0,0 +1,41 @@
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+function print(x) {
+  console.log(x);
+}
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+process.stdin.on("end", () => {
+  inputString = inputString.split("\n");
+  main();
+});
+function readline() {
+  return inputString[currentLine++];
+}
+
+// ********** Code Start **********
+
+function main() {
+  let t = parseInt(readline());
+  for (let i = 0; i < t; i++) {
+    let word = readline().trim();
+    let sword = word.split("").sort().join("");
+    let s = new Set(word.split(""));
+    if (s.size === 1 || word.length === 1) {
+      print("NO");
+    } else {
+      print("YES");
+      if (sword != word) {
+        print(sword);
+      } else {
+        print(sword.split("").reverse().join(""));
+      }
+    }
+  }
+}
