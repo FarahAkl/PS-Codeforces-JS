613de365eb4e0a98f5b4626b2cff9f372003d322
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-02-19 18:57:13 +0200

    add files

---
+++ A - Minutes Before the New Year/main.js
@@ -0,0 +1,30 @@
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+function print(x) {
+  console.log(x);
+}
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+process.stdin.on("end", () => {
+  inputString = inputString.split("\n");
+  main();
+});
+function readline() {
+  return inputString[currentLine++];
+}
+
+// ********** Code Start **********
+
+function main() {
+  let t = parseInt(readline());
+  for (let i = 0; i < t; i++){
+    let [ h, m ] = readline().trim().split(" ").map(Number)
+    print((23-h)*60 + (60-m))
+  }
+}
