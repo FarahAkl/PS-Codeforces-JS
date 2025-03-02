bd44d69920c9506616b8b19715893012120589ab
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-03-02 05:47:40 +0200

    add files

---
+++ A - A+B/main.js
@@ -0,0 +1,30 @@
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+function print(x) {
+  console.log(x);
+}
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+process.stdin.on("end", () => {
+  inputString = inputString.split("\n");
+  main();
+});
+function readline() {
+  return inputString[currentLine++];
+}
+
+// ********** Code Start **********
+
+function main() {
+    let t =parseInt(readline());
+    for (let i = 0; i < t; i++){
+        let [ a, b ] = readline().split("+").map(Number);
+        print(a+b)
+    }
+}
