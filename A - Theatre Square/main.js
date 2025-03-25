86474fb889c179461481b6be1a0f554e9bacb280
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-03-25 20:28:25 +0200

    add files

---
+++ A - Theatre Square/main.js
@@ -0,0 +1,27 @@
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+function print(x) {
+  console.log(x);
+}
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+process.stdin.on("end", () => {
+  inputString = inputString.split("\n");
+  main();
+});
+function readline() {
+  return inputString[currentLine++];
+}
+ 
+// ********** Code Start **********
+
+function main() {
+    let [ n, m, a ] = readline().split(" ").map(Number);
+    print(Math.ceil(n / a) * Math.ceil(m / a));
+}
